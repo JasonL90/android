808622da5bee12b962e6856ecc2c58a1971ba780
JasonL90 <calloponia@gmail.com>
2016-08-17 21:20:42 +0900

    third commit

--- app/src/main/java/com/example/ldw_hlab/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ldw_hlab/myapplication/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.ldw_hlab.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button enter_btn;
+    Button back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
